672d0361f06ba66773e3d515d0ce18a1ed301f4b
cmq2080 <1421224516@qq.com>
2018-08-18 22:00:01 +0800

    feat: 登录页 (refs: #180818-2159)

--- assets/js/common.js
+++ assets/js/common.js
@@ -49,6 +49,7 @@ function ajaxPost(requestUrl, data) {
 	});
 }
 
+// 删除单条记录
 function deleteOne(url, id) {
 	if (!window.confirm("确定删除所选信息？")) {
 		return false;
@@ -58,6 +59,7 @@ function deleteOne(url, id) {
 	});
 }
 
+// 获取选中记录的id
 function getSelected() {
 	var checkbox = $("input[type='checkbox'][name='id']");
 	var id = '';
@@ -70,10 +72,11 @@ function getSelected() {
 		alert("请选择有效信息");
 		return false;
 	}
-	id = id.substr(0, id.length - 1);
+	id = id.substr(0, id.length - 1);/*去掉最后一个分号*/
 	return id;
 }
 
+// 删除选中记录
 function deleteSelected(url) {
 	var id = getSelected();
 	if (id == false) {
@@ -87,14 +90,15 @@ $(function () {
 	/*
 	 * ******************************** 前台 ********************************
 	 */
-	var html_height = parseInt($("html").css("height"));
-	var header_height = $(".header").outerHeight(true);
-	var footer_height = $(".footer").outerHeight(true);
-	var content_margins = parseInt($(".content").css("margin-top")) + parseInt($(".content").css("margin-bottom"));
-	var content_paddings = parseInt($(".content").css("padding-top")) + parseInt($(".content").css("padding-bottom"));
-	console.log(content_margins);
-	var height = html_height - header_height - footer_height - content_margins - content_paddings;
-	$(".content").css({ "min-height": height + "px" });
+	// var html_height = parseInt($("html").css("height"));
+	// var header_height = $(".header").outerHeight(true);
+	// var footer_height = $(".footer").outerHeight(true);
+	// var content_margins = parseInt($(".content").css("margin-top")) + parseInt($(".content").css("margin-bottom"));
+	// var content_paddings = parseInt($(".content").css("padding-top")) + parseInt($(".content").css("padding-bottom"));
+	// console.log(content_margins);
+	// var height = html_height - header_height - footer_height - content_margins - content_paddings;
+	// $(".content").css({ "min-height": height + "px" });
+	
 
 	/*
 	 * ******************************** 后台 ********************************
@@ -105,22 +109,28 @@ $(function () {
 	// 		$(e_tr[i]).css({"background":"#ECECEC"});
 	// 	}
 	// }
-
-	$(".hy-main-table input.id").on("click", function () {
-		if ($(this).is(":checked")) {
-			$(this).attr("checked", "checked");
-		} else {
-			$(this).removeAttr("checked");
-			$(".hy-main-table input.dataid").removeAttr("checked");
-		}
-	});
-
+	// 将框架的最小高度设为屏幕高度（撑屏）
+	height = window.screen.height;
+	console.log(height);
+	$("#iframe").css({ "min-height": height * 0.87 - 37 + "px" });
+	
+	// 总checkbox选中事件
 	$(".hy-main-table input.dataid").on('click', function () {
 		var e_id = $(".hy-main-table input.id");
 		if ($(this).is(":checked")) {
 			$(e_id).prop("checked", "checked");
 		} else {
 			$(e_id).prop("checked", false);
+		}
+	});
+
+	// 分checkbox选中事件
+	$(".hy-main-table input.id").on("click", function () {
+		if ($(this).is(":checked")) {
+			$(this).prop("checked", "checked");
+		} else {
+			$(this).removeAttr("checked");
+			$(".hy-main-table input.dataid").removeAttr("checked");
 		}
 	});
 });
